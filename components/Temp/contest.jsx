6f6c185596072fac9502b1a2f2f6bf67933f01dd
sonneko <152277125+sonneko@users.noreply.github.com>
2024-07-26 08:34:23 +0900

    n

--- components/Temp/contest.jsx
+++ components/Temp/contest.jsx
@@ -1,17 +1,27 @@
 
+/*
+    necessary data set;
+        <MemberTemp data={{
+            title: "",
+            html: "",
+        }} />
+*/
+
+
 function Contest(props) {
     return (
         <>
             <div class="title">
                 <h1>{props.data.title + "の紹介"}</h1>
                 <p>{props.data.title + "について紹介していきます。"}</p>
-                <hr>
+                <hr />
             </div>
             
             <div class="content">
-                <p>{props.data.}</p>
+                <p>{props.data.html}</p>
             </div>
         </>
     )
 }
 
+export default Contest
---
+++ components/Temp/trivia.jsx
@@ -0,0 +1,21 @@
+
+import 
+
+/*
+    necessary data set;
+        <MemberTemp data={{
+            title: "",
+            id: "NUMBER",
+            html: "",
+        }} />
+*/
+
+
+function Trivia(props) {
+    const id = props.data.id
+    return (
+        <h1>{"毎日雑学no." + id}</h1>
+        {if (id <= )}
+        <p></p>
+    )
+}
